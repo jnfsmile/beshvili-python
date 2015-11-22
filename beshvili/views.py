d3d344dc45107c4fc5d62765a40e06dd810c9dac
JNF <jnfsmile@gmail.com>
2015-11-23 00:11:30 +0200

    Trying to add entries to homepage

--- beshvili/views.py
+++ beshvili/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from zinnia.models.entry import Entry
+
 def index(request):
     template = loader.get_template('beshvili/index.html')
-    context = RequestContext(request, {
+    context = RequestContext(request, {'entry_list': Entry.published.all()
     })
     return HttpResponse(template.render(context))
